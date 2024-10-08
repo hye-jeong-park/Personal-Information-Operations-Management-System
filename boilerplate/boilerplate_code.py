053ad4cfcc7b50e2576386de04447c24f183e0a5
hye-jeong-park <park061138@gmail.com>
2024-10-09 00:00:00 +0900

    feat: add functionality to request posts list page

--- boilerplate/boilerplate_code.py
+++ boilerplate/boilerplate_code.py
@@ -17,3 +17,8 @@ print(f"로그인 상태: {response.status_code}")
 response = session.get('https://gw.com2us.com/portal.nsf')
 soup = BeautifulSoup(response.text, 'html.parser')
 print(f"게시글 목록 페이지 상태: {response.status_code}")
+
+# 게시글 목록 페이지 요청
+response = session.get('https://gw.com2us.com/portal.nsf')
+soup = BeautifulSoup(response.text, 'html.parser')
+print(f"게시글 목록 페이지 상태: {response.status_code}")
