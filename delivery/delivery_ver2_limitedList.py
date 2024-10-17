bc9ca4a6f27a6a77b6a2657f67eea5870cc6a224
hye-jeong-park <park061138@gmail.com>
2024-10-18 00:00:00 +0900

    feat: add file information extraction utility

--- delivery/delivery_ver2_limitedList.py
+++ delivery/delivery_ver2_limitedList.py
@@ -18,3 +18,34 @@ def extract_corporate_name(full_text):
     if ',' in full_text:
         return full_text.split(',')[0].split()[0]
     return full_text.split()[0]
+
+def extract_file_info(file_info):
+    """
+    파일형식 및 파일 용량 추출
+    예시: "(Confidential)_20241017_103738_smon_lms_target_list.zip (221KB)"
+    """
+    file_match = re.match(r'\(.*?\)_(.*?)\.(zip|xlsx).*?\((\d+KB)\)', file_info)
+    if file_match:
+        filename = file_match.group(1) + '.' + file_match.group(2)
+        file_size = file_match.group(3)
+        if filename.endswith('.zip'):
+            file_type = 'Zip'
+        elif filename.endswith('.xlsx'):
+            file_type = 'Excel'
+        else:
+            file_type = ''
+        return file_type, file_size
+    else:
+        # 다른 형식의 파일명이 있을 경우
+        p_tags = file_info.split('\n')
+        if len(p_tags) >= 2:
+            filename = p_tags[0].strip()
+            file_size = p_tags[1].strip()
+            if filename.endswith('.zip'):
+                file_type = 'Zip'
+            elif filename.endswith('.xlsx'):
+                file_type = 'Excel'
+            else:
+                file_type = ''
+            return file_type, file_size
+    return '', ''
