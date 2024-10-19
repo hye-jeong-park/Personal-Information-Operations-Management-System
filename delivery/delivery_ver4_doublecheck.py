7d0b1384a03f124edfc337767e104ed444594044
hye-jeong-park <park061138@gmail.com>
2024-10-20 00:00:00 +0900

    chore: add initial constants and configurations

---
+++ delivery/delivery_ver4_doublecheck.py
@@ -0,0 +1,19 @@
+import os
+import re
+import sys
+import time
+import traceback
+import getpass
+import pandas as pd
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from openpyxl import load_workbook
+
+# 크롤링할 게시글 개수 설정 (첫 번째 게시글 제외)
+CRAWL_LIMIT = 10
+
+# 엑셀 파일 경로 및 워크시트 이름 설정
+excel_file = r'C:\Users\PHJ\output\개인정보 운영대장.xlsx'
+worksheet_name = '개인정보 추출 및 이용 관리'
